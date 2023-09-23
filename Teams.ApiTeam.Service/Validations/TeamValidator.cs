52575060faa763d8c58e37927356ef45a59e9359
hacksJuanDavid <josuacoty000@gmail.com>
2023-09-23 03:27:59 -0500

    Update Team validators

--- Teams.ApiTeam.Service/Validations/TeamValidator.cs
+++ Teams.ApiTeam.Service/Validations/TeamValidator.cs
@@ -13,24 +13,16 @@ public class TeamValidator : AbstractValidator<TeamDto>
 
         RuleFor(m => m.Name)
             .NotEmpty()
-            .MaximumLength(50) // Maximum length of 50 characters
-            .Must(BeValidString) // Validate that the string is not null or empty and that it contains only letters or white spaces
+            .MaximumLength(50)
             .WithMessage("Name must be a valid string with a maximum length of 50 characters.");
 
         RuleFor(m => m.Coach)
             .NotEmpty()
-            .MaximumLength(50) // Maximum length of 50 characters 
-            .Must(BeValidString) // Validate that the string is not null or empty and that it contains only letters or white spaces
+            .MaximumLength(50)
             .WithMessage("Coach must be a valid string with a maximum length of 50 characters.");
 
         RuleFor(m => m.Conference)
             .MaximumLength(20)
             .WithMessage("The maximum length of Conference is 20 characters.");
-    }
-
-    private static bool BeValidString(string value)
-    {
-        // Validate that the string is not null or empty and that it contains only letters or white spaces
-        return !string.IsNullOrEmpty(value) && value.All(char.IsLetterOrDigit);
     }
 }
