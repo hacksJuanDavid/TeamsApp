3577b382784fb112e5f1121cc7e182644ce236e8
hacksJuanDavid <josuacoty000@gmail.com>
2023-09-29 20:06:25 -0500

    Update string connection databases and user

--- Scripts/DbTeam/02_Create_User_Team.sql
+++ Scripts/DbTeam/02_Create_User_Team.sql
@@ -1,4 +1,4 @@
 /*DROP USER 'teamsserviceuser'@'localhost' ;*/
-CREATE USER 'teamsserviceuser'@'localhost' IDENTIFIED BY 'M34msS3rv1c3Us3r*01';
-GRANT ALL PRIVILEGES ON *.* TO 'teamsserviceuser'@'localhost' WITH GRANT OPTION;
+CREATE USER 'servicetemasuser'@'localhost' IDENTIFIED BY 'T3M34s4msUs3r*01';
+GRANT ALL PRIVILEGES ON *.* TO 'servicetemasuser'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
--- Scripts/DbTeamMember/02_Create_User_TeamMember.sql
+++ Scripts/DbTeamMember/02_Create_User_TeamMember.sql
@@ -1,4 +1,4 @@
-/*DROP USER 'membersservicedb'@'localhost' ;*/
-CREATE USER 'membersservicedb'@'localhost' IDENTIFIED BY 'T3M34s4msUs3r*01';
-GRANT ALL PRIVILEGES ON *.* TO 'membersservicedb'@'localhost' WITH GRANT OPTION;
+/*DROP USER 'membersservice'@'localhost' ;*/
+CREATE USER 'membersserviceuser'@'localhost' IDENTIFIED BY 'M34msS3rv1c3Us3r*01';
+GRANT ALL PRIVILEGES ON *.* TO 'membersserviceuser'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
